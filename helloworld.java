4db5ad8309cf8cfac47608364efd4b426ce989c2
Asikaprakash <147024211+Asikaprakash@users.noreply.github.com>
2025-04-06 22:00:05 +0530

    Day1

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class helloworld {
+	
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+			// TODO Auto-generated constructor stub
+		}
+
+
